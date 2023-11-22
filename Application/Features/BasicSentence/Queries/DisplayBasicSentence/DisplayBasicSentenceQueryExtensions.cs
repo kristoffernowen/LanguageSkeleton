a6c03795337f04f8972effcd2f93908d7f12a934
Kristoffer Nowén <kristoffer.nowen@learnet.se>
2023-11-22 17:09:55 +0100

    Move some logic into an extension method of the query request

---
+++ Application/Features/BasicSentence/Queries/DisplayBasicSentence/DisplayBasicSentenceQueryExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel;
+using Domain.Enums;
+using Domain.Models.Sentence;
+using Domain.Models.Words;
+
+namespace Application.Features.BasicSentence.Queries.DisplayBasicSentence;
+
+public static class DisplayBasicSentenceQueryExtensions
+{
+    public static Sentence ToSentence(this DisplayBasicSentenceQuery request, Noun subjectNoun, Verb predicate)
+    {
+        var sentence = new Sentence();
+        subjectNoun.GrammaticalNumber = request.SubjectGrammaticalNumber switch
+        {
+            "singular" => GrammaticalNumber.Singular,
+            "plural" => GrammaticalNumber.Plural,
+            _ => throw new InvalidEnumArgumentException()
+        };
+        subjectNoun.Definiteness = request.SubjectDefiniteness switch
+        {
+            "definite" => Definiteness.Definite,
+            "indefinite" => Definiteness.Indefinite,
+            _ => throw new InvalidEnumArgumentException()
+        };
+        sentence.SubjectNoun = subjectNoun;
+        sentence.Tense = request.Tense switch
+        {
+            "present" => Tense.Present,
+            "perfect" => Tense.Perfect,
+            "future" => Tense.Future,
+            "past" => Tense.Past,
+            _ => throw new InvalidEnumArgumentException()
+        };
+        sentence.Predicate = predicate;
+        sentence.StatementOrQuestion = request.StatementOrQuestion switch
+        {
+            "statement" => StatementOrQuestion.Statement,
+            "question" => StatementOrQuestion.Question,
+            _ => throw new InvalidEnumArgumentException()
+        };
+
+        return sentence;
+    }
+}
--- Application/Features/BasicSentence/Queries/DisplayBasicSentence/DisplayBasicSentenceQueryHandler.cs
+++ Application/Features/BasicSentence/Queries/DisplayBasicSentence/DisplayBasicSentenceQueryHandler.cs
@@ -2,8 +2,6 @@
 using Application.Contracts.Services.Noun;
 using Application.Contracts.Services.Sentence;
 using Application.Contracts.Services.Verb;
-using Domain.Enums;
-using Domain.Models.Sentence;
 using MediatR;
 
 namespace Application.Features.BasicSentence.Queries.DisplayBasicSentence
@@ -35,54 +33,16 @@ namespace Application.Features.BasicSentence.Queries.DisplayBasicSentence
         }
         public async Task<DisplayBasicSentenceDto> Handle(DisplayBasicSentenceQuery request, CancellationToken cancellationToken)
         {
-            var sentence = new Sentence();
+            var sentence = request.ToSentence(await _nounService.GetAsync(request.SubjectId), await _verbService.GetAsync(request.PredicateId));
 
-            sentence.SubjectNoun = await _nounService.GetAsync(request.SubjectId);
-            sentence.SubjectNoun.GrammaticalNumber = request.SubjectGrammaticalNumber switch
-            {
-                "singular" => GrammaticalNumber.Singular,
-                "plural" => GrammaticalNumber.Plural,
-                _ => throw new InvalidEnumArgumentException()
-            };
-            sentence.SubjectNoun.Definiteness = request.SubjectDefiniteness switch
-            {
-                "definite" => Definiteness.Definite,
-                "indefinite" => Definiteness.Indefinite,
-                _ => throw new InvalidEnumArgumentException()
-            };
             sentence.SubjectNoun = _nounService.GrammaticalNumberDisplayForm(sentence.SubjectNoun);
             sentence.SubjectNoun = _definitenessService.SetDefinitenessDisplayForm(sentence.SubjectNoun);
-
-            sentence.Predicate = await _verbService.GetAsync(request.PredicateId);
-
-
-            //Fix better please
-
-            sentence.Tense = request.Tense switch
-            {
-                "present" => Tense.Present,
-                "perfect" => Tense.Perfect,
-                "future" => Tense.Future,
-                "past" => Tense.Past,
-                _ => throw new InvalidEnumArgumentException()
-            };
-
-            //
-
-
-
             sentence.Predicate = request.Tense switch
             {
                 "present" => _presentTenseService.SetDisplayForm(sentence.Predicate),
                 "past" => _pastTenseService.SetDisplayForm(sentence.Predicate),
                 "perfect" => _perfectTenseService.SetDisplayForm(sentence.Predicate),
                 "future" => _futureTenseService.SetDisplayForm(sentence.Predicate),
-                _ => throw new InvalidEnumArgumentException()
-            };
-            sentence.StatementOrQuestion = request.StatementOrQuestion switch
-            {
-                "statement" => StatementOrQuestion.Statement,
-                "question" => StatementOrQuestion.Question,
                 _ => throw new InvalidEnumArgumentException()
             };
             sentence = await _wordOrderService.ToQuestionOrStatementAsync(sentence);
